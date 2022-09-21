b5cd11f0271377a41f78e975813232a52e04c0c2
Jon Hammond <jrhammond09@gmail.com>
2022-09-21 02:49:13 -0400

    js file cleanup

--- script.js
+++ script.js
@@ -4,13 +4,8 @@ const authorText = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
 
-// let apiQuotes = [];
-
-// Show new quote
 function newQuote() {
-  //  Pick a random quote from apiQuotes array
   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-  // Display author/quote on page
   authorText.textContent = quote.author;
 
   if (quote.text.length > 50) {
@@ -21,28 +16,12 @@ function newQuote() {
   quoteText.textContent = quote.text;
 }
 
-// Tweet quote
 function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
   window.open(twitterUrl, '_blank');
 }
 
-// Event listeners for Twitter and new quote buttons
 newQuoteButton.addEventListener('click', newQuote);
 twitterButton.addEventListener('click', tweetQuote);
 
-// Get quotes from API
-// async function getQuotes() {
-//   const apiUrl = 'https://type.fit/api/quotes';
-//   try {
-//     const response = await fetch(apiUrl);
-//     apiQuotes = await response.json();
-//     newQuote();
-//   } catch (error) {
-//     // Catch error here
-//   }
-// }
-
-// On load
-// getQuotes();
 newQuote();
